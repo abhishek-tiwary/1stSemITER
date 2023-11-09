495d29d1817080bcc7cde95890a1ae90e5ce5cf4
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2023-11-10 01:18:07 +0530

    A01- Q3

---
+++ Assignment01/Q3_HashPattern.java
@@ -0,0 +1,13 @@
+public class HashPattern {
+    public static void main(String[] args) {
+		// Name- Abhishek Tiwary
+		// Registration No.- 2341020019
+	        System.out.println("##########");
+			System.out.println("##      ##");
+			System.out.println("##      ##");
+			System.out.println("##########");
+
+	}
+
+}
+
