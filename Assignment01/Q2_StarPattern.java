7d66461e166767ab3d99c7fa7a9de929bdc8cccb
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2023-11-10 01:13:52 +0530

    A01- Q1, Q2

---
+++ Assignment01/Q2_StarPattern.java
@@ -0,0 +1,12 @@
+public class StarPattern {
+    public static void main(String[] args) {
+		// Name- Abhishek Tiwary
+		// Registration No.- 2341020019
+			System.out.println("*******     *******       ");
+			System.out.println("      *     *     *       ");
+			System.out.println("      *******     *******");
+					
+
+	}
+
+}
