1c17e01b8045638bff28245ff22133aa489004b8
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2023-11-13 11:20:20 +0530

    A01- Q5

---
+++ Assignment01/Q5_Greetings.java
@@ -0,0 +1,15 @@
+public class Greetings {
+    public static void main(String[] args) {
+    // Name- Abhishek Tiwary
+    // Registration No.- 2341020019
+                String name = "Abhishek";
+                String greeting = " Good morning";
+                
+                System.out.println("Hey" + " " + name + "," + greeting +"!");
+    }            
+}
+
+
+
+    
+
