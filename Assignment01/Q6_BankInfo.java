8bbb7d4f0ba4014a86a853bf8e1b001509f3adce
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2023-11-14 14:48:59 +0530

    A01- Q6

---
+++ Assignment01/Q6_BankInfo.java
@@ -0,0 +1,11 @@
+public class BankInfo {
+    public static void main(String[] args) {
+		// Name- Abhishek Tiwary
+		// Registration No.- 2341020019
+		String name = "Abhishek Tiwary";
+		String accNo = "123456";
+		float balance = 7654.98f;
+							
+		System.out.println("\"My name is " + name + ", bearing account number " + accNo  + " having balance " + balance + "\"");
+    }
+}
