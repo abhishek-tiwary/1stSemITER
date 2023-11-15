eef92dd547cac4538e670830aa97eef1d0855602
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2023-11-15 20:39:01 +0530

    A01- Q7

---
+++ Assignment01/Q7_SwapValues.java
@@ -0,0 +1,16 @@
+public class SwapValues {
+    public static void main(String[] args) {
+		//Declaring variable
+		int x= 10;
+		int y = 11;
+		int z;
+		System.out.println("x = " + x + " y = " +y);
+		//Swapping process
+		z=x;
+		x=y;
+		y=z;
+		System.out.println("x = " + x + " y = " + y);
+
+	}
+    
+}
