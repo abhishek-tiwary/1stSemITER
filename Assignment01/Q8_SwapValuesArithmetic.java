702d5a5925fa0d2012f34af93f286278b7d09bb3
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2023-11-16 19:35:52 +0530

    A01- Q8

---
+++ Assignment01/Q8_SwapValuesArithmetic.java
@@ -0,0 +1,20 @@
+public class SwapValuesArithmetic {
+
+	public static void main(String[] args) {
+		//Declaring variable
+		int x= 10;
+		int y = 11;
+		System.out.println("x = " + x + " y = " +y);
+		//Swapping process
+		 x=x+y;
+		 y=x-y;
+		 x=x-y;
+		 /*Can also do it by using multiplication and division
+		  * x= x*y;
+		  * y=x/y;
+		  * x=x/y; */
+		System.out.println("x = " + x + " y = " + y);
+
+	}
+
+}
