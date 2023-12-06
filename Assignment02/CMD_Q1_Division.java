884198c373206dc8bb09e7b29d0ca6d6d270673e
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2023-12-06 22:06:46 +0530

    A02- CMD- 01

---
+++ Assignment02/CMD_Q1_Division.java
@@ -0,0 +1,17 @@
+public class CMD_Q1_Division {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("a= "+ a+ "\tb= " +b);
+		float c=(float) a/b;
+		System.out.println("c= "+c);
+		
+		/* Right click on program space
+		 * Run As --> Run Congurations
+		 * Check class,project, main class, name properly
+		 * Click on arguments
+		 * Pass reqd no. of values in program arguments (separate by spaces) & click run
+		*/ 
+	}
+
+}
