d1d1c68ccb2af74232964812fa32559af85f33de
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2024-01-30 00:30:07 +0530

    A03- H02

---
+++ Assignment 03/HW_Q2_Calendar.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class HW_Q2_Calendar {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter today's day");
+        int day= sc.nextInt();
+        System.out.println("Enter the number of days elapsed since today");
+        int elapsed= sc.nextInt();
+
+        String today = "";
+        String futureDay= "";
+
+        switch(day){
+            case 0 -> today = "Sunday";
+            case 1 -> today = "Monday";
+            case 2 -> today = "Tuesday";
+            case 3 -> today = "Wednesday";
+            case 4 -> today = "Thursday";
+            case 5 -> today = "Friday";
+            case 6 -> today = "Saturday";
+        }
+
+        switch((day + elapsed)%7){
+            case 0 -> futureDay = "Sunday";
+            case 1 -> futureDay = "Monday";
+            case 2 -> futureDay = "Tuesday";
+            case 3 -> futureDay = "Wednesday";
+            case 4 -> futureDay = "Thursday";
+            case 5 -> futureDay = "Friday";
+            case 6 -> futureDay = "Saturday";
+        }
+
+        System.out.println("Today is " + today + " and the future day is " + futureDay);
+        sc.close();
+    }
+}
