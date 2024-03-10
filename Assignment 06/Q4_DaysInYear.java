693277557271b67d5eaf8bec76d24019ab456351
Abhishek Tiwary <abhishek.tiwary1574@gmail.com>
2024-03-10 23:46:06 +0530

    A06- Q4

---
+++ Assignment 06/Q4_DaysInYear.java
@@ -0,0 +1,15 @@
+public class Q4_DaysInYear {
+    public static void main(String[] args) {
+        for (int i = 2000; i <= 2020; i++) {
+            System.out.println("Number of days in year " + i + " is " + numberOfDaysInAYear(i) );
+        }
+    }
+
+    public static int numberOfDaysInAYear(int year) {
+        return isLeapYear(year) ? 366 : 365;
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
